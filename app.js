773220ffbbfa020f6afa85b5466e34c19e8a2294
captainquestion <canberk.yilmaz439@gmail.com>
2023-04-16 19:51:53 -0400

    Updated ports and procfile

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.get("/contact", function (req, res) {
 });
 
 
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
-});
+app.listen(process.env.PORT || 3000, function () { 
+  console.log("Server started.");
+   }); 
 
